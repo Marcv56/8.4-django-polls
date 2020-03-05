1316a52525194080bbb967ff77de62bf4713e1f3
Marcos <marcv56@hotmail.com>
2020-03-05 11:59:46 -0500

    code refactor

--- polls/urls.py
+++ polls/urls.py
@@ -5,8 +5,13 @@ from . import views
 app_name ='polls'
 
 urlpatterns = [
+    # these are the urls for each "view" to be displayed
     path('', views.IndexView.as_view(), name='index'),
-    path('/<int:pk>/', views.DetailView.as_view(), name='detail'),
-	path('/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-	path('/<int:question_id>/vote/', views.vote, name='vote'),
+    # index is the main view that shows the questions
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # Detail view shows the questions and the bouttons for voting
+	path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # this will show the results for what the user selected
+	path('<int:question_id>/vote/', views.vote, name='vote'),
+    # this gives the ability to vote
 ]
